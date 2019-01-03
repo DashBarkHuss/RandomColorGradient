30acbf538953d296bd5aaa90df4dce90ad9fc42d
dash <dangerousdashie@gmail.com>
2019-01-03 10:31:26 -0500

    refactored and does from memory

--- script.js
+++ script.js
@@ -1,28 +1,98 @@
+
 var c;
 var ctx;
 var grd;
 
-window.onload = function () {
+window.onload = function(){
     c = document.getElementById("canvas");
-    ctx = c.getContext("2d");
-    grd=ctx.createLinearGradient(0,150,300,150);
-
-};
+    ctx= c.getContext('2d');
+    
+    grd = ctx.createLinearGradient(0,150,300,150)
+}
 
 function getRandomColor(){
-    function randomHue(){
-       return Math.floor(Math.random()*361);
-    };
-    function randomLightness(){
-        return Math.floor(Math.random()*41 + 50);
-     };
-    return  'hsl(' + randomHue() + ',100%,' +  + randomLightness() +'%)';
-};
+    var hue = Math.floor(Math.random()*361);
+    var lightness = Math.floor(Math.random()*41+50);
+
+    return 'hsl(' + hue + ',100%,' + lightness +'%)';
+}
+
 
 function addGradient(){
-    color = getRandomColor();
-    position = Math.random();
+    var color = getRandomColor();
+    var position = Math.random();
+
     grd.addColorStop(position, color);
+
     ctx.fillStyle = grd;
-    ctx.fillRect(0, 0, 300, 300);
+    ctx.fillRect(0,0, 300, 300);
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var c;
+// var ctx;
+// var grd;
+
+// window.onload = function () {
+//     c = document.getElementById("canvas");
+//     ctx = c.getContext("2d");
+//     grd=ctx.createLinearGradient(0,150,300,150);
+
+// };
+
+// function getRandomColor(){
+//     function randomHue(){
+//        return Math.floor(Math.random()*361);
+//     };
+//     function randomLightness(){
+//         return Math.floor(Math.random()*41 + 50);
+//      };
+//     return  'hsl(' + randomHue() + ',100%,' +  + randomLightness() +'%)';
+// };
+
+// function addGradient(){
+//     color = getRandomColor();
+//     position = Math.random();
+//     grd.addColorStop(position, color);
+//     ctx.fillStyle = grd;
+//     ctx.fillRect(0, 0, 300, 300);
+// }
